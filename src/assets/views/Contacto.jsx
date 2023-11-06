9ee6931e308d2f04801ec84a2bc2ab4873ad6509
sarebok <bortonae@hotmail.com>
2023-11-06 09:09:56 -0300

    add a little css

--- src/assets/views/Contacto.jsx
+++ src/assets/views/Contacto.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Contacto = () => {
   return (
-    <div>
+    <div className="contacto">
       <h1>Cuentanos, en que te podemos ayudar?</h1>
       <form>
         <input type="text" placeholder="ingresa tu correo:" />
         <input type="text" placeholder="cuentanos en que podemos ayudar" />
+        <button>Enviar</button>
       </form>
     </div>
   );
--- src/assets/views/Home.jsx
+++ src/assets/views/Home.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <h1>Bienvenido a Happy Cake</h1>
+      <img src="https://th.bing.com/th?id=OIP.DaVxeM7HxPNxQmdnWnV3GgHaJ6&w=216&h=289&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
     </div>
   );
 };
